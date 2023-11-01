b347a03ee3fa973b811c2edd141758203843b4be
Samruddhi-22 <samruddhiingole77@gmail.com>
2023-11-01 08:54:39 +0530

    Update hello world program for readability and maintainability

--- practicals/hello.php
+++ practicals/hello.php
@@ -1,3 +1,11 @@
 <?php
-echo "Hello!,world";
+// A simple "Hello, World!" program in PHP
+
+// Define a function to display the message
+function sayHello() {
+    echo "Hello, World!";
+}
+
+// Call the function to display the message
+sayHello();
 ?>
