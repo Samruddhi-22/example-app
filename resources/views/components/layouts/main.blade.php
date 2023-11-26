85890006db3b9287b743a352d09a8ab4d6b8f27b
Samruddhi-22 <samruddhiingole77@gmail.com>
2023-11-27 00:12:38 +0530

    Update songs-list.blade.php to render songs from database using a @foreach loop over $songs

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/layouts/main.css') }}">
-    <title>{{ $title ?? "Cool Web App" }}</title>
+    <title>{{ $title ?? "Cool Web App" }} </title>
 </head>
 
 <body>
@@ -26,32 +26,11 @@
                 <li>Playlist 3</li>
             </ul>
         </aside>
-
-        <section>
-            <div>
-                <div>Serial No</div>
-                <div>Song Title</div>
-                <div>Artist Name</div>
-                <div>Album Name</div>
-                <div>Duration</div>
-            </div>
-            <div>
-                <div>1</div>
-                <div>Song 1</div>
-                <div>Artist 1</div>
-                <div>Album 1</div>
-                <div>3:30</div>
-            </div>
-            <div>
-                <div>2</div>
-                <div>Song 2</div>
-                <div>Artist 2</div>
-                <div>Album 2</div>
-                <div>4:15</div>
-            </div>
-            <!-- Add more rows as needed -->
-        </section>
-    </main>
+        
+       @yield('content')
+        
+         
+</main>
 
     <footer>
         <p>&copy; 2023 Playlist App | <a href="#">About</a> | <a href="#">Contact</a></p>
